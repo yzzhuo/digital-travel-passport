0b760b468781d149b1c3777c4db2962c1e4e8add
junezhuo <yzzhuo2@gmail.com>
2023-11-07 09:35:42 +0200

    fix: build error

--- src/utils/uploader.ts
+++ src/utils/uploader.ts
@@ -2,7 +2,7 @@
 // https://cloudinary.com/documentation/upload_images#example_1_upload_multiple_files_using_a_form_unsigned
 const url = 'https://api.cloudinary.com/v1_1/hzxyensd5/image/upload'
 
-export const uploadFiles = async (files: File[]) => {
+export const uploadFiles = async (files: FileList) => {
   const formData = new FormData()
   for (let i = 0; i < files.length; i++) {
     const file = files[i]
